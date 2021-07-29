ad59d4a12d011929ab9c01fb829ee8573dc01be7
farrarj86 <54745917+farrarj86@users.noreply.github.com>
2021-07-29 16:12:21 -0400

    Fix MokListRT error

--- install.sh
+++ install.sh
@@ -1,8 +1,13 @@
 #!/bin/sh
 PACKAGES="broadcom-sta-dkms ubuntustudio-installer"
 
-#Installing Broadcom drivers for BCM4321 and ubuntustudio installer
 
+#Installing Broadcom drivers for BCM4321 and ubuntustudio installer
 apt update -y
 apt upgrade -y
 apt install -y $PACKAGES
+
+# Fix "failed to set MOKlislRT" error
+cd /boot/efi/EFI/ubuntu/
+cp grubx64.efi shimx64.efi
+reboot
